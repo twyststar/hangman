d57f27a9a78bb2931e9c415457ca7643a35ba809
Ben Petrosky and Starlynne Start <ben+starlynne@>
2017-04-26 16:40:50 -0700

    Create basic methods and specs for hangman app

---
+++ lib/hangman.rb
@@ -0,0 +1,52 @@
+class Hangman
+  @@words = []
+
+  define_method(:initialize) do |word|
+    @word = word.split("")
+    @id = @@words.length()+1
+    @answer = []
+    @wrong = []
+    @answer = self.blanked()
+  end
+
+  define_method(:blanked) do
+    @word.each() do |letter|
+      @answer.push("_")
+    end
+    @answer
+  end
+
+  define_method(:word) do
+    @word
+  end
+  define_method(:answer) do
+    @answer
+  end
+  define_method(:id) do
+    @id
+  end
+  define_method(:win_check?) do
+    @answer == @word
+  end
+
+  define_method(:letter_check) do |input|
+    i=0
+
+    if @word.include?(input)
+      @word.each() do |letter|
+        if letter == input
+          @answer[i] = input
+          i = i.+(1)
+        else
+          i = i.+(1)
+        end
+      end
+      @answer
+    else
+      @wrong.push(input)
+    end
+  end
+
+
+
+end
---
+++ spec/hangman_spec.rb
@@ -0,0 +1,37 @@
+require('rspec')
+require('hangman')
+require('pry')
+
+describe(Hangman) do
+  describe('initialize') do
+    it('returns new hangman word') do
+      test_word = Hangman.new('hello')
+      expect(test_word.word).to(eq(['h', 'e', 'l', 'l', 'o']))
+    end
+    it('returns new hangman blank answer field') do
+      test_word2 = Hangman.new('hello')
+      expect(test_word2.answer).to(eq(['_', '_', '_', '_', '_']))
+    end
+  end
+  describe('letter_check') do
+    it('fills hangman blank answer field with correctly guessed letter') do
+      test_word3 = Hangman.new('hello')
+      expect(test_word3.letter_check('l')).to(eq(['_', '_', 'l', 'l', '_']))
+    end
+  end
+  describe('letter_check') do
+    it('returns input in wrong answer field with incorrectly guessed letter') do
+      test_word4 = Hangman.new('hello')
+      expect(test_word4.letter_check('q')).to(eq(['q']))
+    end
+  end
+  describe('win_check') do
+    it('returns true if all letters have been guessed') do
+      test_word5 = Hangman.new('hel')
+      test_word5.letter_check('h')
+      test_word5.letter_check('e')
+      test_word5.letter_check('l')
+      expect(test_word5.win_check?()).to(eq(true))
+    end
+  end
+end
